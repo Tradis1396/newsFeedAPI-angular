ae029ab9ef9c73561b25292b3dd779f4451c1ac4
VigneshSE1 <48944738+VigneshSE1@users.noreply.github.com>
2020-02-25 16:05:33 +0530

    fix

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/src/index.html'));
+app.use(express.static(__dirname + '/dist/angular-newsfeed'));
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/src/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/angular-newsfeed/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
